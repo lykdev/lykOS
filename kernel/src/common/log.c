632cab3749dc245be7e13160c50a919e23ef3a2f
Matei Lupu <matei.lupu1104@gmail.com>
2025-08-12 22:27:47 +0300

    LOG: temp solution for video buffer overflow

--- kernel/src/common/log.c
+++ kernel/src/common/log.c
@@ -4,11 +4,12 @@
 #include <common/sync/spinlock.h>
 #include <graphics/draw.h>
 #include <graphics/font_basic.h>
+#include <lib/list.h>
 #include <lib/printf.h>
 #include <lib/string.h>
 
-static int line = 0;
-static spinlock_t slock = SPINLOCK_INIT;
+static u64 g_line = 0;
+static spinlock_t g_slock = SPINLOCK_INIT;
 
 void _log(const char *format, ...)
 {
@@ -22,17 +23,22 @@ void _log(const char *format, ...)
 
 void _n_log(const char *format, va_list list)
 {
-    spinlock_acquire(&slock);
+    spinlock_acquire(&g_slock);
 
     char buf[256] = {0};
     vsnprintf(buf, 256, format, list);
 
     arch_serial_send_str(buf);
-    arch_serial_send_str("\n");
+    if (buf[strlen(buf) - 1] != '\n')
+        arch_serial_send_str("\n");
 
-    for (int i = 0; buf[i] != '\0'; i++)
-        draw_char(&video_fb, i * font_basic.width, line * font_basic.height, buf[i], &font_basic, 0xFFFFFF);
-    line++;
+    // TODO: scroll
+    if (g_line < video_fb.height / font_basic.height)
+    {
+        for (int i = 0; buf[i] != '\0'; i++)
+            draw_char(&video_fb, i * font_basic.width, g_line * font_basic.height, buf[i], &font_basic, 0xFFFFFF);
+        g_line++;
+    }
 
-    spinlock_release(&slock);
+    spinlock_release(&g_slock);
 }
